4088046def248968f56f7c47661ecd326aea8981
Val Lyashov <val@plstr.com>
2012-04-25 14:29:41 +1000

    changed to contain 2 dicts of movies.

--- src/Library/Library.java
+++ src/Library/Library.java
@@ -5,29 +5,32 @@ import java.util.ArrayList;
 
 public class Library {
 
-    // The list of movies. This is lame. TODO: CSV at least.
-    private String[] movieList = new String[]{
-        "The Matrix;Wachowski Brothers;Sci-Fi,Adventure;1998;120;available;true;true",
-        "Star Trek;J.J. Abrams;Sci-Fi,Drama,Romance;2009;120;available;false;true",
-        "Dark City;Alex Proyas;Sci-Fi,Horror;1998;true;120;available;false",
-        "Brotherhood of the Wolf;Christophe Gans;Horror;2001;120;available;true;false",
-        "City of God;Fernando Meirelles;Sci-Fi;2002;120;available;true;true",
-        "Pi;Darren Aronofsky;Thriller;1998;120;available;false;true"
+    // The list of movies. This is lame. TODO: sqlite at least.
+    private String[] digiMovieList = new String[]{
+        // title | director | genre | year | length | cast | size | format | digi_rent | digi_buy
+        "The Matrix;Wachowski Brothers;Sci-Fi,Adventure;1998;120;Addy Ze, Kake Face;512;divX;true;true",
+        "Star Trek;J.J. Abrams;Sci-Fi,Drama,Romance;2009;120;Addy Ze, Kake Face;512;divX;true;true"
     };
 
-    private Movie[] movieLibrary = new Movie[movieList.length];
-    public Movie (String mvID, String title, String director, String genre,
-                  int year, int length, String status, String[] cast) {
+    private String[] phyMovieList = new String[]{
+            // barcode | title | director | genre | year | length | cast | qnt | phy_rent | phy_buy
+            "12321323;The Matrix;Wachowski Brothers;Sci-Fi,Adventure;1998;120;Bobby Dee, Andrew Bob;2;true;false",
+            "12321321;Star Trek;J.J. Abrams;Sci-Fi,Drama,Romance;2009;120;Bobby Dee, Andrew Bob;2;true;false"
+    };
+
+    // init stores
+    private HashMap digiMovieLibrary;
+    private HashMap phyMovieLibrary;
+
     public Library() {
-        // Repo of all the movies
+        // Repos of all the movies
         int mvid = 0;
-        for (int i=0; i <= movieList.length - 1; i++){
-            String[] eachMovie = movieList[i].split(";");
-            if(eachMovie.length == 6){
-                movieLibrary[i] = new Movie(mvid++, eachMovie[0], eachMovie[1],
-                    eachMovie[2], Integer.parseInt(eachMovie[3]),
+        for (String each : digiMovieList){
+            String[] eachMovie = each.split(";");
+            digiMovieLibrary.put(eachMovie[0], Movie(eachMovie[0], eachMovie[1],
+                    eachMovie[2], Integer.parseInt(eachMovie[3]), eachMovie[4],
                     Boolean.parseBoolean(eachMovie[4]),
-                    Boolean.parseBoolean(eachMovie[5]));
+                    Boolean.parseBoolean(eachMovie[5])));
             }
         }
     }
